4b981bfa8da537c099d5035ea9ce999cb444c5e9
vr3n <vickyspatel@gmail.com>
2022-06-16 19:49:33 +0530

    Secret Key Fix

--- todo_backend/settings/ci.py
+++ todo_backend/settings/ci.py
@@ -2,5 +2,5 @@ import os
 
 from .base import *  # noqa: F403 F401
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "i^sb2my@58$m2m)8bm%jssyyqqi#34sd!#4et+_14)j=lp*^x"
 DEBUG = os.environ.get("DEBUG")
