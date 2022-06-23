a6d4c55091dd147770067c871bbad21843fa66e0
vr3n <vickyspatel@gmail.com>
2022-06-23 18:11:22 +0530

    Written tests for Token generation.

--- users/signals.py
+++ users/signals.py
@@ -8,6 +8,5 @@ User = get_user_model()
 
 @receiver(post_save, sender=User)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
-    print('created_auth_token')
     if created:
         Token.objects.create(user=instance)
