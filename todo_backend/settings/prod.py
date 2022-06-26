9dde73063262b292f3b8d3af61687c69e186610a
vr3n <vickyspatel@gmail.com>
2022-06-26 10:58:33 +0530

    CI environment

--- todo_backend/settings/prod.py
+++ todo_backend/settings/prod.py
@@ -1,4 +1,7 @@
+import re
 from .base import *
+
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -12,3 +15,45 @@ DATABASES = {
         'PORT': ''
     }
 }
+
+CSRF_COOKIE_SECURE = True
+SEESION_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "root": {"level": "INFO", "handlers": ["file"]},
+    "handlers": {
+        "file": {
+            "level": "INFO",
+            "class": "logging.FileHandler",
+            "filename": "/var/log/django.log",
+            "formatter": "app",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": "INFO",
+            "propagate": True
+        },
+    },
+    "formatters": {
+        "app": {
+            "format": (
+                u"%(asctime)s [%(levelname)-8s] "
+                "(%(module)s.%(funcName)s) %(message)s"
+            ),
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+        },
+    },
+}
+
+IGNORABLE_404_URLS = [
+    re.compile(r'\.(php|cgi)$'),
+    re.compile(r'^/phpmyadmin/'),
+    re.compile(r'^/apple-touch-icon.*\.png$'),
+    re.compile(r'^/favicon\.ico$'),
+    re.compile(r'^/robots\.txt$'),
+]
